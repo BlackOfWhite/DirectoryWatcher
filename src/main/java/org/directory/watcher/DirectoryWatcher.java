e070bb84d12b6bd7adbe8fab9e73e2cb44534b16
BlackOfWhite <niewinskipiotr1994@gmail.com>
2018-11-28 21:52:21 +0100

    Support for multiple file filters. Test cases for file manipulation and filters.

--- src/main/java/org/directory/watcher/DirectoryWatcher.java
+++ src/main/java/org/directory/watcher/DirectoryWatcher.java
@@ -1,241 +1,234 @@
 package org.directory.watcher;
 
-import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
-
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
+import static java.nio.file.StandardWatchEventKinds.*;
+
 public class DirectoryWatcher implements DirectoryWatcherCallback, Runnable {
 
-  public static final String ALLOW_ALL = "ALLOW_ALL";
-  private static final String THREAD_ID = "DirectoryWatcherRunnable";
-  private static Logger logger = LoggerFactory.getLogger(DirectoryWatcher.class);
-  private final boolean recursive;
-  private Path rootPath;
-  private WatchService service;
-  private Map<WatchKey, Path> keys;
-  private WatchEvent.Kind[] WATCH_EVENTS = new WatchEvent.Kind[]{ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE};
-  private int maxDepth;
-  private String fileFilter;
-  private Thread thread;
-  private boolean notifyDirectories;
-  private volatile boolean running;
+    private static final String THREAD_ID = "DirectoryWatcherRunnable";
+    private static Logger logger = LoggerFactory.getLogger(DirectoryWatcher.class);
+    private final boolean recursive;
+    private Path rootPath;
+    private WatchService service;
+    private Map<WatchKey, Path> keys;
+    private WatchEvent.Kind[] WATCH_EVENTS = new WatchEvent.Kind[]{ENTRY_CREATE, ENTRY_MODIFY, ENTRY_DELETE};
+    private int maxDepth;
+    private List<String> fileFilters;
+    private Thread thread;
+    private boolean notifyDirectories;
+    private volatile boolean running;
 
-  public DirectoryWatcher(Path rootPath, boolean recursive) throws IOException {
-    this(rootPath, recursive, Integer.MAX_VALUE, ALLOW_ALL);
-  }
-
-  public DirectoryWatcher(Path rootPath, boolean recursive, int maxDepth, String fileFilter) throws IOException {
-    this.rootPath = rootPath;
-    this.service = FileSystems.getDefault().newWatchService();
-    this.recursive = recursive;
-    this.keys = new HashMap<>();
-    this.maxDepth = maxDepth;
-    this.fileFilter = fileFilter;
-    this.thread = new Thread(this, "");
-    this.notifyDirectories = true;
-  }
-
-  @SuppressWarnings("unchecked")
-  static <T> WatchEvent<T> cast(WatchEvent<?> event) {
-    return (WatchEvent<T>) event;
-  }
-
-  public void start() {
-    try {
-      this.running = true;
-      this.thread.start();
-    } catch (IllegalStateException e) {
-      logger.warn("DirectoryWatcher was already started.", e);
+    public DirectoryWatcher(Path rootPath, boolean recursive) throws IOException {
+        this(rootPath, recursive, Integer.MAX_VALUE, Collections.emptyList());
     }
-  }
 
-  public void close() {
-    this.running = false;
-  }
+    public DirectoryWatcher(Path rootPath, boolean recursive, int maxDepth, List<String> fileFilters) throws IOException {
+        this.rootPath = rootPath;
+        this.service = FileSystems.getDefault().newWatchService();
+        this.recursive = recursive;
+        this.keys = new HashMap<>();
+        this.maxDepth = maxDepth;
+        this.fileFilters = fileFilters;
+        this.thread = new Thread(this, "");
+        this.notifyDirectories = true;
+    }
 
-  /**
-   * Register the given directory with the WatchService
-   */
-  private void register(Path dir) throws IOException {
-    WatchKey key = dir.register(service, WATCH_EVENTS);
-    keys.put(key, dir);
-  }
+    @SuppressWarnings("unchecked")
+    static <T> WatchEvent<T> cast(WatchEvent<?> event) {
+        return (WatchEvent<T>) event;
+    }
 
-  /**
-   * Register the given directory, and all its sub-directories, with the
-   * WatchService.
-   */
-  private void registerAll(final Path start) throws IOException {
-    // register directory and sub-directories
-    Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
-      @Override
-      public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-          throws IOException {
-        if (Files.isDirectory(dir)) {
-          register(dir);
-          callback(dir, ENTRY_CREATE);
+    public void start() {
+        try {
+            this.running = true;
+            this.thread.start();
+        } catch (IllegalStateException e) {
+            logger.warn("DirectoryWatcher was already started.", e);
         }
-        return FileVisitResult.CONTINUE;
-      }
-    });
-  }
+    }
 
-  /**
-   * Process all events for keys queued to the watcher
-   */
-  void processEvents() {
-    while (running) {
-      WatchKey key;
-      try {
-        key = service.take();
-      } catch (InterruptedException e) {
-        logger.warn("Unexpected exception while running {} thread.", THREAD_ID, e);
+    public void close() {
         this.running = false;
-        return;
-      }
+    }
 
-      Path dir = keys.get(key);
-      if (dir == null) {
-        continue;
-      }
+    /**
+     * Register the given directory with the WatchService
+     */
+    private void register(Path dir) throws IOException {
+        WatchKey key = dir.register(service, WATCH_EVENTS);
+        keys.put(key, dir);
+    }
 
-      for (WatchEvent<?> event : key.pollEvents()) {
-        WatchEvent.Kind kind = event.kind();
+    /**
+     * Register the given directory, and all its sub-directories, with the
+     * WatchService.
+     */
+    private void registerAll(final Path start) throws IOException {
+        // register directory and sub-directories
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                    throws IOException {
+                if (Files.isDirectory(dir)) {
+                    register(dir);
+                    callback(dir, ENTRY_CREATE);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
 
-        // Context for directory entry event is the file name of entry
-        WatchEvent<Path> ev = cast(event);
-        Path name = ev.context();
-        Path child = dir.resolve(name);
+    /**
+     * Process all events for keys queued to the watcher
+     */
+    void processEvents() {
+        while (running) {
+            WatchKey key;
+            try {
+                key = service.take();
+            } catch (InterruptedException e) {
+                logger.warn("Unexpected exception while running {} thread.", THREAD_ID, e);
+                this.running = false;
+                return;
+            }
 
-        if (Files.isDirectory(child, NOFOLLOW_LINKS) && recursive && (kind == ENTRY_CREATE) && getPathDepth(child) < maxDepth) {
-          try {
-            registerAll(child);
-          } catch (IOException e) {
-            logger.warn("Failed to access directory: {}", child, e);
-          }
-        } else if (isFileAllowed(child)) { // callbacks for files from the root level
-          callback(child, kind);
+            Path dir = keys.get(key);
+            if (dir == null) {
+                continue;
+            }
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                WatchEvent.Kind kind = event.kind();
+
+                // Context for directory entry event is the file name of entry
+                WatchEvent<Path> ev = cast(event);
+                Path name = ev.context();
+                Path child = dir.resolve(name);
+
+                if (Files.isDirectory(child, NOFOLLOW_LINKS) && recursive && (kind == ENTRY_CREATE) && getPathDepth(child) < maxDepth) {
+                    try {
+                        registerAll(child);
+                    } catch (IOException e) {
+                        logger.warn("Failed to access directory: {}", child, e);
+                    }
+                } else if (isFileAllowed(child)) { // callbacks for files from the root level
+                    callback(child, kind);
+                }
+            }
+
+            // reset key and remove from set if directory no longer accessible
+            boolean valid = key.reset();
+            if (!valid) {
+                keys.remove(key);
+                // all directories are inaccessible
+                if (keys.isEmpty()) {
+                    break;
+                }
+            }
         }
-      }
+        logger.info("{} was successfully stopped.", THREAD_ID);
+    }
 
-      // reset key and remove from set if directory no longer accessible
-      boolean valid = key.reset();
-      if (!valid) {
-        keys.remove(key);
-        // all directories are inaccessible
-        if (keys.isEmpty()) {
-          break;
+    private int getPathDepth(Path path) {
+        if (!path.toAbsolutePath().startsWith(rootPath.toAbsolutePath())) {
+            return -1;
         }
-      }
+        return path.getNameCount() - rootPath.getNameCount();
     }
-    logger.info("{} was successfully stopped.", THREAD_ID);
-  }
 
-  private int getPathDepth(Path path) {
-    if (!path.toAbsolutePath().startsWith(rootPath.toAbsolutePath())) {
-      return -1;
+    private boolean isFileAllowed(Path file) {
+        return fileFilters.isEmpty() || fileFilters.stream().anyMatch(s -> file.toString().endsWith(s));
     }
-    return path.getNameCount() - rootPath.getNameCount();
-  }
 
-  private boolean isFileAllowed(Path file) {
-    return fileFilter.equals(ALLOW_ALL) || file.toString().endsWith(fileFilter);
-  }
-
-  public int getMaxDepth() {
-    return maxDepth;
-  }
-
-  public void setMaxDepth(int maxDepth) {
-    this.maxDepth = maxDepth;
-  }
-
-  public String getFileFilter() {
-    return fileFilter;
-  }
-
-  public void setFileFilter(String fileFilter) {
-    this.fileFilter = fileFilter;
-  }
-
-  public boolean isRecursive() {
-    return recursive;
-  }
-
-  public Path getRootPath() {
-    return rootPath;
-  }
-
-  public boolean isNotifyDirectories() {
-    return notifyDirectories;
-  }
-
-  public void setNotifyDirectories(boolean notifyDirectories) {
-    this.notifyDirectories = notifyDirectories;
-  }
-
-  public boolean isRunning() {
-    return running;
-  }
-
-  @Override
-  public void run() {
-    try {
-      if (recursive) {
-        registerAll(rootPath);
-      } else {
-        register(rootPath);
-      }
-    } catch (IOException e) {
-      logger.warn("DirectoryWatcher was unable to register path: {}", e.getMessage(), e);
-      Thread.currentThread().interrupt();
-      return;
+    public int getMaxDepth() {
+        return maxDepth;
     }
-    processEvents();
-  }
 
-  @Override
-  public void pathCreated(Path path) {
-    logger.debug("Path created.");
-  }
-
-  @Override
-  public void pathModified(Path path) {
-    logger.debug("Path modified.");
-  }
-
-  @Override
-  public void pathRemoved(Path path) {
-    logger.debug("Path removed.");
-  }
-
-  private void callback(Path path, WatchEvent.Kind kind) {
-    if (Files.isDirectory(path) && !notifyDirectories) {
-      logger.debug("Notifications for directories are off.");
+    public void setMaxDepth(int maxDepth) {
+        this.maxDepth = maxDepth;
     }
-    if (kind == ENTRY_CREATE) {
-      pathCreated(path);
-    } else if (kind == ENTRY_MODIFY) {
-      pathModified(path);
-    } else if (kind == ENTRY_DELETE) {
-      pathRemoved(path);
-    } else {
+
+    public List<String> getFileFilters() {
+        return fileFilters;
     }
-  }
+
+    public void setFileFilters(List<String> fileFilters) {
+        this.fileFilters = fileFilters;
+    }
+
+    public boolean isRecursive() {
+        return recursive;
+    }
+
+    public Path getRootPath() {
+        return rootPath;
+    }
+
+    public boolean isNotifyDirectories() {
+        return notifyDirectories;
+    }
+
+    public void setNotifyDirectories(boolean notifyDirectories) {
+        this.notifyDirectories = notifyDirectories;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (recursive) {
+                registerAll(rootPath);
+            } else {
+                register(rootPath);
+            }
+        } catch (IOException e) {
+            logger.warn("DirectoryWatcher was unable to register path: {}", e.getMessage(), e);
+            Thread.currentThread().interrupt();
+            return;
+        }
+        processEvents();
+    }
+
+    @Override
+    public void pathCreated(Path path) {
+        logger.debug("Path created.");
+    }
+
+    @Override
+    public void pathModified(Path path) {
+        logger.debug("Path modified.");
+    }
+
+    @Override
+    public void pathRemoved(Path path) {
+        logger.debug("Path removed.");
+    }
+
+    private void callback(Path path, WatchEvent.Kind kind) {
+        if (Files.isDirectory(path) && !notifyDirectories) {
+            logger.debug("Notifications for directories are off.");
+        }
+        if (kind == ENTRY_CREATE) {
+            pathCreated(path);
+        } else if (kind == ENTRY_MODIFY) {
+            pathModified(path);
+        } else if (kind == ENTRY_DELETE) {
+            pathRemoved(path);
+        } else {
+        }
+    }
 }
--- src/test/java/DirectoryWatcherTest.java
+++ src/test/java/DirectoryWatcherTest.java
@@ -1,18 +1,3 @@
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
-import static org.directory.watcher.DirectoryWatcher.ALLOW_ALL;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchEvent.Kind;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
 import org.directory.watcher.DirectoryWatcher;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
@@ -21,110 +6,211 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchEvent.Kind;
+import java.util.*;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
 public class DirectoryWatcherTest {
 
-  private static final Path resourcesDirectory = Paths.get(new File("src/test/resources").getAbsolutePath());
-  private static Logger logger = LoggerFactory.getLogger(DirectoryWatcherTest.class);
-  private List<PathEvent> eventList = Collections.synchronizedList(new ArrayList<>());
+    private static final Path resourcesDirectory = Paths.get(new File("src/test/resources").getAbsolutePath());
+    private static Logger logger = LoggerFactory.getLogger(DirectoryWatcherTest.class);
+    private List<PathEvent> eventListCreate = Collections.synchronizedList(new ArrayList<>());
+    private List<PathEvent> eventListModify = Collections.synchronizedList(new ArrayList<>());
+    private List<PathEvent> eventListDelete = Collections.synchronizedList(new ArrayList<>());
+    Path testLevel23_txt = Paths.get(resourcesDirectory.toString(), "testDir11", "testLevel23.txt");
+    Path testLevel23_png = Paths.get(resourcesDirectory.toString(), "testDir11", "testLevel23.png");
 
-  @Before
-  public void setUp() {
-    eventList.clear();
-  }
-
-  @Test
-  public void overrideAndInitialize() {
-    try {
-      DirectoryWatcher watcher = getWatcher();
-      Assert.assertNotNull(watcher);
-      Assert.assertEquals(watcher.isRecursive(), true);
-      Assert.assertEquals(watcher.getFileFilter(), ALLOW_ALL);
-      Assert.assertEquals(watcher.getMaxDepth(), Integer.MAX_VALUE);
-      Assert.assertEquals(watcher.isNotifyDirectories(), true);
-      watcher.start();
-      Thread.sleep(3000);
-      Assert.assertEquals(eventList.size(), 4);
-      Assert.assertThat(eventList, CoreMatchers
-          .hasItems(new PathEvent(resourcesDirectory, ENTRY_CREATE),
-              new PathEvent(Paths.get(resourcesDirectory.toString(), "testDir11"), ENTRY_CREATE),
-              new PathEvent(Paths.get(resourcesDirectory.toString(), "testDir12"), ENTRY_CREATE),
-              new PathEvent(Paths.get(resourcesDirectory.toString(), "testDir11", "testDir21"), ENTRY_CREATE)));
-
-    } catch (IOException | InterruptedException e) {
-      e.printStackTrace();
-    }
-  }
-
-  @Test
-  public void startAndStopTest() {
-    try {
-      DirectoryWatcher watcher = getWatcher();
-      watcher.start();
-      Assert.assertTrue(watcher.isRunning());
-      Thread.sleep(1000);
-      watcher.close();
-      Assert.assertFalse(watcher.isRunning());
-    } catch (IOException | InterruptedException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private DirectoryWatcher getWatcher() throws IOException {
-    return new DirectoryWatcher(resourcesDirectory, true) {
-      @Override
-      public void pathCreated(Path path) {
-        logger.info("Path created: " + path);
-        eventList.add(new PathEvent(path, ENTRY_CREATE));
-      }
-
-      @Override
-      public void pathModified(Path path) {
-        logger.info("Path modified: " + path);
-        eventList.add(new PathEvent(path, ENTRY_MODIFY));
-      }
-
-      @Override
-      public void pathRemoved(Path path) {
-        logger.info("Path removed: " + path);
-        eventList.add(new PathEvent(path, ENTRY_DELETE));
-      }
-    };
-  }
-
-  private class PathEvent {
-
-    private Path path;
-    private WatchEvent.Kind kind;
-
-    public PathEvent(Path path, Kind kind) {
-      this.path = path;
-      this.kind = kind;
+    @Before
+    public void setUp() {
+        testLevel23_txt.toFile().delete();
+        testLevel23_png.toFile().delete();
+        eventListCreate.clear();
+        eventListModify.clear();
+        eventListDelete.clear();
     }
 
-    public Path getPath() {
-      return path;
+    @Test
+    public void overrideAndInitialize() {
+        try {
+            DirectoryWatcher watcher = getWatcher();
+            startWatcherWithDelay(watcher);
+
+            Assert.assertNotNull(watcher);
+            Assert.assertEquals(watcher.isRecursive(), true);
+            Assert.assertTrue(watcher.getFileFilters().isEmpty());
+            Assert.assertEquals(watcher.getMaxDepth(), Integer.MAX_VALUE);
+            Assert.assertEquals(watcher.isNotifyDirectories(), true);
+            Assert.assertEquals(eventListCreate.size(), 4);
+            Assert.assertThat(eventListCreate, CoreMatchers
+                    .hasItems(new PathEvent(resourcesDirectory, ENTRY_CREATE),
+                            new PathEvent(Paths.get(resourcesDirectory.toString(), "testDir11"), ENTRY_CREATE),
+                            new PathEvent(Paths.get(resourcesDirectory.toString(), "testDir12"), ENTRY_CREATE),
+                            new PathEvent(Paths.get(resourcesDirectory.toString(), "testDir11", "testDir21"), ENTRY_CREATE)));
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public Kind getKind() {
-      return kind;
+    @Test
+    public void startAndStopTest() {
+        try {
+            DirectoryWatcher watcher = getWatcher();
+            watcher.start();
+            Assert.assertTrue(watcher.isRunning());
+            Thread.sleep(1000);
+            watcher.close();
+            Assert.assertFalse(watcher.isRunning());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-        return false;
-      }
-      PathEvent pathEvent = (PathEvent) o;
-      return Objects.equals(path, pathEvent.path) &&
-          Objects.equals(kind, pathEvent.kind);
+    @Test
+    public void manipulateSingleFileTest() throws IOException, InterruptedException {
+        DirectoryWatcher watcher = getWatcher();
+        startWatcherWithDelay(watcher);
+
+        File file = testLevel23_txt.toFile();
+        file.createNewFile();
+        assertTrue(file.exists());
+        Thread.sleep(500);
+        Assert.assertThat(eventListCreate, CoreMatchers
+                .hasItems(new PathEvent(file.toPath(), ENTRY_CREATE)));
+        file.delete();
+        assertFalse(file.exists());
+        Thread.sleep(500);
+        Assert.assertThat(eventListModify, CoreMatchers
+                .hasItems(new PathEvent(file.toPath(), ENTRY_MODIFY)));
+        Assert.assertThat(eventListDelete, CoreMatchers
+                .hasItems(new PathEvent(file.toPath(), ENTRY_DELETE)));
     }
 
-    @Override
-    public int hashCode() {
-      return Objects.hash(path, kind);
+    @Test
+    public void singleFileFilter() throws IOException, InterruptedException {
+        DirectoryWatcher watcher = getWatcher(Integer.MAX_VALUE, Arrays.asList(".png"));
+        startWatcherWithDelay(watcher);
+
+        File file = testLevel23_txt.toFile();
+        File file2 = testLevel23_png.toFile();
+        file.createNewFile();
+        file2.createNewFile();
+        assertTrue(file.exists() && file2.exists());
+        Thread.sleep(500);
+        Assert.assertFalse(eventListCreate.contains(new PathEvent(file.toPath(), ENTRY_CREATE)));
+        Assert.assertTrue(eventListCreate.contains(new PathEvent(file2.toPath(), ENTRY_CREATE)));
+        file.delete();
+        file2.delete();
+        assertTrue(!file.exists() && !file2.exists());
+        Thread.sleep(500);
+        Assert.assertFalse(eventListModify.contains(new PathEvent(file.toPath(), ENTRY_MODIFY)));
+        Assert.assertFalse(eventListDelete.contains(new PathEvent(file.toPath(), ENTRY_DELETE)));
+        Assert.assertThat(eventListModify, CoreMatchers
+                .hasItems(new PathEvent(file2.toPath(), ENTRY_MODIFY)));
+        Assert.assertThat(eventListDelete, CoreMatchers
+                .hasItems(new PathEvent(file2.toPath(), ENTRY_DELETE)));
     }
-  }
+
+    private DirectoryWatcher getWatcher() throws IOException {
+        return new DirectoryWatcher(resourcesDirectory, true) {
+            @Override
+            public void pathCreated(Path path) {
+                logger.info("Path created: " + path);
+                eventListCreate.add(new PathEvent(path, ENTRY_CREATE));
+            }
+
+            @Override
+            public void pathModified(Path path) {
+                logger.info("Path modified: " + path);
+                eventListModify.add(new PathEvent(path, ENTRY_MODIFY));
+            }
+
+            @Override
+            public void pathRemoved(Path path) {
+                logger.info("Path removed: " + path);
+                eventListDelete.add(new PathEvent(path, ENTRY_DELETE));
+            }
+        };
+    }
+
+    private DirectoryWatcher getWatcher(int depth, List<String> fileFilters) throws IOException {
+        return new DirectoryWatcher(resourcesDirectory, true, depth, fileFilters) {
+            @Override
+            public void pathCreated(Path path) {
+                logger.info("Path created: " + path);
+                eventListCreate.add(new PathEvent(path, ENTRY_CREATE));
+            }
+
+            @Override
+            public void pathModified(Path path) {
+                logger.info("Path modified: " + path);
+                eventListModify.add(new PathEvent(path, ENTRY_MODIFY));
+            }
+
+            @Override
+            public void pathRemoved(Path path) {
+                logger.info("Path removed: " + path);
+                eventListDelete.add(new PathEvent(path, ENTRY_DELETE));
+            }
+        };
+    }
+
+    private DirectoryWatcher startWatcherWithDelay(DirectoryWatcher watcher) throws IOException, InterruptedException {
+        watcher.start();
+        Thread.sleep(500); // only for test purposes
+        return watcher;
+    }
+
+    private class PathEvent {
+
+        private Path path;
+        private WatchEvent.Kind kind;
+
+        public PathEvent(Path path, Kind kind) {
+            this.path = path;
+            this.kind = kind;
+        }
+
+        public Path getPath() {
+            return path;
+        }
+
+        public Kind getKind() {
+            return kind;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            PathEvent pathEvent = (PathEvent) o;
+            return Objects.equals(path, pathEvent.path) &&
+                    Objects.equals(kind, pathEvent.kind);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(path, kind);
+        }
+
+        @Override
+        public String toString() {
+            return "PathEvent{" +
+                    "path=" + path +
+                    ", kind=" + kind +
+                    '}';
+        }
+    }
 }
